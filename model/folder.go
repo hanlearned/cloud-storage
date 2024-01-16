7ecc011894a0079da52f7e9c2bfa902c023aa923
hanlearned <54702352+hanlearned@users.noreply.github.com>
2024-01-16 21:53:58 +0800

    init code

--- model/folder.go
+++ model/folder.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"cloud-storage/model/mysql"
-	"fmt"
 )
 
 type Folder struct {
@@ -20,11 +19,36 @@ func CreateFolder(name string, folderId int, warehouseId int, included int) (boo
 		WareHouseId: warehouseId,
 		Included:    included,
 	}
-	fmt.Println(folder)
+	// 判断父级文件夹是否存在
+	folderExist := QueryFolderByID(folderId)
+	if folderExist == false {
+		return false, folder
+	}
 	result := mysql.DB.Create(&folder)
 	if result.Error != nil {
-		fmt.Println(result.Error)
 		return false, folder
 	}
 	return true, folder
+}
+
+func QueryFolderByID(folderId int) bool {
+	// 判断文件夹是否存在
+	if folderId == 0 {
+		return true
+	}
+	folder := Folder{}
+	mysql.DB.Where("id = ?", folderId).Find(&folder)
+	if folder.ID == 0 {
+		return false
+	}
+	return true
+}
+
+func DeleteFolder(folderId int) bool {
+	folder := Folder{}
+	result := mysql.DB.Where("id = ?", folderId).Delete(&folder)
+	if result.Error != nil {
+		return false
+	}
+	return true
 }
